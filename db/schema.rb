deec489367bc2b862bf601c57994ed94a25fa549
Kento Kurahashi <kuraken0128@gmail.com>
2017-10-21 07:25:59 +0000

    kiso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021064827) do
+ActiveRecord::Schema.define(version: 20171021072157) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
